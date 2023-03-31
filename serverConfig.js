52030b5e20071d572ca2c81f2364d0c7239dc43b
RamR3R <sendtoramsundar@gmail.com>
2023-03-31 05:40:53 +0530

    checking using get admin in api

--- serverConfig.js
+++ serverConfig.js
@@ -2,6 +2,6 @@ export default {
   protectedRoutes: [
     { route: "/users", methods: ["PUT", "DELETE", "PATCH"] },
     { route: "/DFabrica", methods: ["POST","PUT", "DELETE", "PATCH"] },
-    { route: "/admin", methods:["GET","POST","PUT", "DELETE", "PATCH"]}
+    { route: "/admin", methods:["POST","PUT", "DELETE", "PATCH"]}
   ]
 }
